ea007442fc647fb9d1e8798294424f8d552966df
max <ruanjiayou123@gmail>
2026-05-10 14:16:26 +0800

    改为nginx处理

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,6 @@ import { configure } from 'mobx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import DrawerMenu from './components/DrawerMenu'
 // import reportWebVitals from './reportWebVitals';
 configure({
   enforceActions: 'never',
@@ -10,7 +9,6 @@ configure({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DrawerMenu isMain={true}/>
     <App />
   </React.StrictMode>
 );
