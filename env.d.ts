ef400cf9fb1239c7e86f977987c613fe89e54c47
阮家友 <ruanjiayou123@gamil.com>
2026-05-11 14:59:31 +0800

    更新types

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,25 @@
+import { Request as RequestBase,Response } from 'express';
+
+declare module 'express-serve-static-core' {
+  interface Response {
+    success(data?: any): void;
+    fail(message?: string, code?: number): void;
+  }
+}
+
+declare global {
+  namespace Express {
+    interface Request extends RequsetBase {
+      paging(): { limit: number, page: number, limit: number };
+    };
+  }
+}
+
 declare module "bun" {
   interface Env {
     PORT: string;
     DATABASE_DIR: string;
   }
 }
+
+export {};
